f1f191d29221a372a3c97f6f658ad2d1e273626e
Urban, Karol (GDT-BESD) <Karol.Urban@bshg.com>
2022-08-19 12:10:07 +0000

    data read ready

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -3,8 +3,13 @@
 from sklearn.model_selection import train_test_split
 
 # Add the necessary imports for the starter code.
-
+import pandas as pd
+import os
 # Add code to load in the data.
+
+dir_path = os.path.dirname(__file__).split('/starter')[0]
+path_to_data = dir_path + '/starter/data/census.csv'
+data = pd.read_csv(filepath_or_buffer = path_to_data)
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
